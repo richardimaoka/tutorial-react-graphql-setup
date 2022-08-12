4b8328b6245714400be1b9acf0bd6e82d135469a
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-08-13 05:33:30 +0900

    empty App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,11 @@
-import React from "react";
-import logo from "./logo.svg";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
 
-export default App;
+export const App = (): JSX.Element => {
+  return <ApolloProvider client={client}>empty</ApolloProvider>;
+};
